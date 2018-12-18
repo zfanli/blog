a3cee7bf11d0a7b10543ee65d85a825af7245ccf
Richard <cyanyao@live.com>
2018-12-18 13:16:05 +0800

    refactor: make all posts export as a module

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './posts'
+import './pages'
 import './utils/ga'
 
 Vue.config.productionTip = false
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import store from './store'
-import { IMPORT_POST_DYNAMIC } from './actions'
+import store from '../store'
+import { IMPORT_POST_DYNAMIC } from '../actions'
 
 // list of post file name
 const list = [
   '2017-10-16-css3-transition&transform',
-  '2017-10-07-try-of-REST-api',
+  '2017-10-07-RESTful-api-java',
 ]
 
 list.map(l => store.dispatch(IMPORT_POST_DYNAMIC, l))

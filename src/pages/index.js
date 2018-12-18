19e7cfff9b054496c16e0ad3271f7a989696c567
Richard <cyanyao@live.com>
2018-12-18 18:08:00 +0800

    update: update list

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,17 @@ import { IMPORT_POST_DYNAMIC } from '../actions'
 
 // list of post file name
 const list = [
+  '2018-02-08-css-flexbox',
+  '2018-01-28-memo-python',
+  '2017-11-26-memo-lock',
+  '2017-10-29-memo-vba',
   '2017-10-16-css3-transition&transform',
+  '2017-10-08-caution-about-aspect',
   '2017-10-07-RESTful-api-java',
+  '2017-10-06-memo-spring-security',
+  '2017-09-30-memo-log4j',
+  '2017-09-19-memo-mybatis',
+  '2017-09-10-memo-spring-mvc',
 ]
 
 list.map(l => store.dispatch(IMPORT_POST_DYNAMIC, l))

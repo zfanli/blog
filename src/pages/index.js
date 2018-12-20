b718d872f8842c0a61913bc837de3b6d103badb0
Richard <cyanyao@live.com>
2018-12-20 18:06:27 +0800

    update: list new posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import { IMPORT_POST_DYNAMIC } from '../actions'
 
 // list of post file name
 const list = [
+  '2018-06-05-simple-usage-about-mongodb',
+  '2018-05-30-restful-api-python',
   '2018-02-08-css-flexbox',
   '2018-01-28-memo-python',
   '2017-11-26-memo-lock',
   '2017-10-29-memo-vba',
   '2017-10-16-css3-transition&transform',
   '2017-10-08-caution-about-aspect',
-  '2017-10-07-RESTful-api-java',
+  '2017-10-07-restful-api-java',
   '2017-10-06-memo-spring-security',
   '2017-09-30-memo-log4j',
   '2017-09-19-memo-mybatis',

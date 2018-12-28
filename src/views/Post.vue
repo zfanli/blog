28c8b0ef9677976f373c2ab23b272de5832cab82
Richard <cyanyao@live.com>
2018-12-29 00:42:08 +0800

    update: use white background when zooms images

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -129,7 +129,7 @@ export default {
       }
     },
     registerImageZoomer() {
-      zoomer('img', { background: '#000000cc' })
+      zoomer('img', { background: '#fff' })
     },
   },
   watch: {
